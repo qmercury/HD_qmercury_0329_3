73f1be9e55ef3d74af78f5056e49239d8502dd6b
qmercury <qmercury@knou.ac.kr>
2022-03-30 18:33:03 +0900

    유니티스터디 0329 연습문제 3번

---
+++ HD_qmercury_0329_3.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HD_qmercury_0329_3.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 0;
+            for (int i = 1; i < 11; i++) 
+            {
+                Console.WriteLine((2 * i) - 1);
+                j = j + (2*i)-1;
+            }
+            Console.WriteLine($"Sum is {j}");
+                    
+        }
+    }
+}
+// 1~20까지 수 중에서 홀수 값의 합을 출력하기
